be24c07d2c222440109640e72fdf913d86572868
hollabaq86 <hollis65@me.com>
2018-07-01 15:17:27 -0400

    dedupe tests pass

--- lib/picker.rb
+++ lib/picker.rb
@@ -1,8 +1,21 @@
 class Picker
 	attr_reader :input, :group_size
+	attr_accessor :output
 
 	def initialize(input, group_size)
 		@input = input
 		@group_size = group_size
+	end
+
+	def do_the_thing()
+		deduped = dedupe(@input)
+	end
+
+	def dedupe(source_data)
+		working_data = []
+		source_data.each do |item|
+			working_data << item if !working_data.include?(item)
+		end
+		return working_data
 	end
 end
--- spec/picker_spec.rb
+++ spec/picker_spec.rb
@@ -15,26 +15,26 @@ describe Picker do
 	describe '.do_the_thing' do
 
 		before(:all) do
-			picker = Picker.new(['john', 'jane', 'jack', 'jack'], 2)
-			picker.do_the_thing()
+			@picker = Picker.new(['john', 'jane', 'jack', 'jack'], 2)
+			@picker.do_the_thing()
+		end
+		
+		it 'dedupes the list of entries' do
+			expect(@picker.output.flatten.length).to eql(3)
+			expect(@picker.output.select{ |name| name == 'jack' }.length).to eql(1)
 		end
 		
 		it 'creates and array of arrays' do
-			expect(picker.output).to be_an(Array)
-			picker.output.each { |group| expect(group).to be_an(Array)}
+			expect(@picker.output).to be_an(Array)
+			@picker.output.each { |group| expect(group).to be_an(Array)}
 		end
 
 		it 'creates groups according to a specified number of entries' do
-			expect(picker.output[0].length).to eql(2)
-		end
-
-		it 'dedupes the list of entries' do
-			expect(picker.output.flatten.length).to eql(3)
-			expect(picker.output.select{ |name| name == 'jack' }.length).to eql(1)
+			expect(@picker.output[0].length).to eql(2)
 		end
 
 		it 'returns one group with less than the requested number of entries if the total number of entries does not divide evenly by the requested number' do
-			expect(picker.output.last).to eql(1)
+			expect(@picker.output.last).to eql(1)
 		end
 
 		it 'creates groups that are random' do
@@ -43,8 +43,8 @@ describe Picker do
 			#expect the final number to be less than the total number of attempts
 			jack_count = 0
 			100.times do
-				picker.do_the_thing()
-				jack_count += 1 if picker.output.first.first == 'jack'
+				@picker.do_the_thing()
+				jack_count += 1 if @picker.output.first.first == 'jack'
 			end
 			
 			expect(jack_count).to be < 100
@@ -52,8 +52,9 @@ describe Picker do
 
 
 		it 'returns all groups' do
-			expect(picker.output.length).to be(2)
+			expect(@picker.output.length).to be(2)
 		end
 
 	end
+
 end
