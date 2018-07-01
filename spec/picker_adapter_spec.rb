1588f35567b025588af8b7d7305ea67b32b09907
hollabaq86 <hollis65@me.com>
2018-07-01 14:39:13 -0400

    add tests for PickerAdapter

--- spec/picker_adapter_spec.rb
+++ spec/picker_adapter_spec.rb
@@ -1,23 +1,42 @@
 require 'rspec'
 require 'picker_adapter'
+require 'csv'
 
 describe PickerAdapter do
 
 	describe '.input_prepare' do
 
-		it 'converts a CSV of entries into an object' do
+		file = CSV.generate do |csv|
+			csv << ["john"]
+			csv << ["jane"]
+			csv << ["jack"]
 		end
 
-		it 'returns the object and number of desired entries per group' do
+		it 'converts a CSV of entries into an Array of entries' do
+			converted_data = PickerAdapter.input_prepare(file)
+			expect(converted_data).to be_an(Array) 
+			expect(converted_data.length).to eql(3)
 		end
+
 	end
 
 	describe '.output_prepare' do
 
-		it 'converts an object of groups to a CSV' do
+		let (:groups) {[
+			["john", "jack"],
+			["jane"]
+		]}
+		let (:result) {[["john","jack"],["jane"]]}
+
+		it 'converts an Array of groups to a CSV' do
+			PickerAdapter.out_prepare(groups)
+			CSV.foreach("results.csv") do |row|
+				expect(result.include?(row)).to be(true) 
+			end
 		end
 
 		it 'returns the name of output CSV' do
+			expect(PickerAdapter.output_prepare(groups)).to eql("results.csv")		
 		end
 	end
 
